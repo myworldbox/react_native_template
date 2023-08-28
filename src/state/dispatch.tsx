417831d22b1f56411d0a0b8dfb0c5643419bde42
Victor Leung <victor.leung@M-FINANCE.NET>
2023-08-28 16:12:23 +0800

    %npm_config_comment%

--- src/state/dispatch.tsx
+++ src/state/dispatch.tsx
@@ -1,15 +1,13 @@
 const dispatch = (callback: any, value: Object) => {
 
-    var type = Object.keys(value)[0]
+    var type = `${Object.keys(value)}`
 
     var content = Object.values(value)[0]
 
     Object.keys(content).map((path: any) => {
-        console.log(path, "8787")
         
     content[`${type}.${path}`] = content[path]
     delete content[path]
-    console.log(content)
     callback({ type: type, payload: content });
     })
 };
